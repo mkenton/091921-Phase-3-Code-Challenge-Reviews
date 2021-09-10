6b5cc8d8ff9deabaf4e3fb9147ed9a4c20f231b0
Michael <mkenton2020@icloud.com>
2021-09-10 11:38:14 -0500

    complete object association methods, add Review#print_review method

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ActiveRecord::Base
-    
+    belongs_to :product
+    belongs_to :user
+
+    def prints_review
+        puts "Review for #{product.name} by #{user.name}: #{self.star_rating}. #{self.comment}" 
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@
 puts "Deleting old data..."
 Product.destroy_all
 User.destroy_all
+Review.destroy_all
 
 puts "Creating users..."
 user1 = User.create(name: Faker::Name.name)
@@ -22,5 +23,17 @@ puts "Creating reviews..."
 # * and a review belongs to an user.                              *
 # *****************************************************************
 # Create Reviews Here
+stapler_u1_r1 = Review.create(star_rating: 4, comment: "I would give 5 stars, but I believe someone has my stapler.", product_id: product1.id, user_id: user1.id)
+stapler_u2_r1 = Review.create(star_rating: 5, comment: "Best stapler! I borrowed it but I like it so much that I don't think I'm going to give it back!", product_id: product1.id, user_id: user2.id)
+stapler_u2_r1 = Review.create(star_rating: 5, comment: "Great pen! I found it on my co-worker's desk!", product_id: product4.id, user_id: user2.id)
 
 puts "Seeding done!"
+puts
+puts "Review#user for first review:"
+puts Review.first.user.name
+puts
+puts "Review#product for first reveiw"
+puts Review.first.product.name
+puts
+puts "Testing all review comments for user 2"
+puts user2.reviews.map(&:comment)
